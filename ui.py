c5e9a578262e100ae89cfc8c16cf0a44d02e90df
yeeyungong <51457638+yeeyungong@users.noreply.github.com>
2024-05-03 00:04:38 +0800

    Update ui.py

--- ui.py
+++ ui.py
@@ -101,16 +101,17 @@ if st.button("Recommend"):
                             # Load the image using PIL
                             image = Image.open(BytesIO(response.content))
                             # Resize the image while maintaining aspect ratio
-                            resized_image = image.resize((350, 350))
+                            resized_image = image.resize((250, 350))
                             # Convert the image to base64 format
                             image_base64 = image_to_base64(resized_image)
                             # Display the resized image with title above
                             st.markdown(f"<div style='text-align:center'><h2>{recommendation['image_title']}</h2>"
                                         f"<div style='margin: 0 auto;'>"
-                                        f"<p style='text-align:center;'>Similarity Score: {recommendation['score']}</p>"
                                         f"<img src='data:image/jpeg;base64,{image_base64}' style='max-width: 250px; max-height: 350px;'>"
-                                        f"<div style='background-color: lightblue; padding: 5px; border-radius: 5px; margin-right: 10px; width: 150px; display:inline-block;'>{recommendation['location']}</div>"
-                                        f"<div style='background-color: lightgreen; padding: 5px; border-radius: 5px; width: 150px; display:inline-block;'>{' '.join(['#' + tag for tag in recommendation['hashtag'].split(', ')])}</div>"
+                                        f"<p style='text-align:center;'>Similarity Score: {recommendation['score']}</p>"
+                                        f"<p style='text-align:center;'>Location: {recommendation['location']}</p>"
+                                        f"<p style='text-align:center;'>Hashtags: {' '.join(['#' + tag for tag in recommendation['hashtag'].split(', ')])}</p>"
+                                        f"</div>"
                                         f"</div>", unsafe_allow_html=True)
                     except Exception as e:
                         st.write(f"Error loading image from URL: {full_image_url}")
