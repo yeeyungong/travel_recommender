ed51c0060b8258611f74a3710f93ae5c6d782dfb
yeeyungong <51457638+yeeyungong@users.noreply.github.com>
2024-05-03 00:14:14 +0800

    Update ui.py

--- ui.py
+++ ui.py
@@ -111,6 +111,8 @@ if st.button("Recommend"):
                     except Exception as e:
                         st.write(f"Error loading image from URL: {full_image_url}")
                         st.write(e)
+                    row_html += "</div>"
+            st.markdown(row_html, unsafe_allow_html=True)
 
     else:
         st.write("No recommendations found based on your input.")
